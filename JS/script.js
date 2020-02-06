f0703ab888979b58edd38a11be07dbc9842987c0
Treinamento <Treinamento@RD723925.raiadrogasil.com>
2020-02-06 16:53:19 -0300

    terminado

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,57 @@
 let btnAddTarefa = document.getElementById("adicionar-tarefa")
 let input = document.getElementById("tarefa-digitada")
+let btnDelet = document.querySelectorAll(".botao-tarefa")
+let tarefas = document.getElementById("tarefas")
+let listaTarefas = localStorage.getItem("listaTarefas") ? JSON.parse(localStorage.getItem("listaTarefas")) : []
 
-    let tarefas = document.getElementById("tarefas")
-    btnAddTarefa.onclick = function (){
-        let valorDigitado=input.value
-        let tarefaNova =`  <div class="col-md-4">
+
+
+const salvarLocalStorage = tarefas => {
+    let myJSON = JSON.stringify(tarefas)
+    localStorage.setItem("listaTarefas", myJSON)
+    console.log("lista de tarefas salva co sucesso")
+}
+
+const mostrarNaTela = (tarefasSalvas) => {
+    tarefasSalvas.forEach(tarefaTexto => {
+
+        let tarefaNova = ` <div class="col-md-4">
+        <div class="box row">
+            <div class="texto-tarefa col-md-10 text-wrap ">
+            ${tarefaTexto}
+            </div>
+            <div class="botao-tarefa col-md-2">
+                <img src="Imagens/check.png" alt="botão para finalizar tarefa" title="feito" width="40vw" />
+            </div>
+        </div>
+    </div>`
+        tarefas.insertAdjacentHTML("beforeend", tarefaNova)
+        let objetoTarefaNova = tarefas.lastElementChild
+        let btnCheck = objetoTarefaNova.lastElementChild.lastElementChild
+        btnCheck.onclick = (event) => {
+            event.target.parentNode.parentNode.parentNode.remove()
+            listaTarefas =listaTarefas.filter(valor=>valor!=tarefaTexto) 
+            salvarLocalStorage(listaTarefas)
+        }
+    })
+
+}
+
+
+mostrarNaTela(listaTarefas)
+
+
+const criarTarefaComEnter = (event) => {
+    if (event.keyCode == 13 || event.type == "click") {
+        let valorDigitado = input.value
+        if (valorDigitado == "") {
+            alert("você precisa digitar uma tarefa primeiro");
+            return
+        }
+        listaTarefas.push(valorDigitado);
+        salvarLocalStorage(listaTarefas);
+        input.value = ""
+        let tarefaNova = `  <div class="col-md-4">
         <div class="box row">
             <div class="texto-tarefa col-md-10 text-wrap ">
             ${valorDigitado}
@@ -14,5 +61,17 @@ let input = document.getElementById("tarefa-digitada")
             </div>
         </div>
     </div>`
-       tarefas.innerHTML += tarefaNova
+        tarefas.insertAdjacentHTML("beforeend", tarefaNova)
+        let objetoTarefaNova = tarefas.lastElementChild
+        let btnCheck = objetoTarefaNova.lastElementChild.lastElementChild
+        btnCheck.onclick = (event) => {
+            event.target.parentNode.parentNode.parentNode.remove()
+            listaTarefas =listaTarefas.filter(valor=>valor!=valorDigitado) 
+            salvarLocalStorage(listaTarefas)
+        }
     }
+}
+
+
+input.addEventListener("keypress", criarTarefaComEnter)
+btnAddTarefa.addEventListener("click", criarTarefaComEnter)
